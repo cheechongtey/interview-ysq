31d3678b7c757b6a7e5d2defb3538596f0829f64
Chee Chong <cheechongtey@gmail.com>
2024-05-19 14:12:13 +0800

    style: add shadcn config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,77 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
-    content: [
-        "./resources/views/app.blade.php",
-        "./resources/app/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            zIndex: {
-                1000: "1000",
-                2000: "2000",
-                5000: "5000",
-                5050: "5050",
-            },
+  darkMode: ['class'],
+  content: ['./resources/views/app.blade.php', './resources/app/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
         },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        error: 'hsl(0, 100%, 63%)',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      backgroundColor: {
+        main: 'hsl(218, 28%, 13%)',
+        'intro-and-email': 'hsl(217, 28%, 15%)',
+        footer: 'hsl(216, 53%, 9%)',
+        testimonials: 'hsl(219, 30%, 18%)',
+      },
+      fontFamily: {
+        header: ["'Raleway', sans-serif", ...defaultTheme.fontFamily.sans],
+        sans: ["'Open Sans', sans-serif", ...defaultTheme.fontFamily.sans],
+      },
     },
-    plugins: [],
+  },
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
-
